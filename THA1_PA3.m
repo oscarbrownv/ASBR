a2e2111d0f3c009a4a7949271780398413e2fa7d
Bugart Lan <bugart@Bugarts-MacBook-Pro-387.local>
2025-02-18 14:41:49 -0600

    fix m_rotm2axang.m singularity

--- THA1_PA3.m
+++ THA1_PA3.m
@@ -1,3 +1,5 @@
+clear; clc; close all;
+
 %% Testing configurations
 q = [0; 2; 0]; % A point on the screw axis
 s = [0; 0; 1]; % Scew axis direction
@@ -6,18 +8,20 @@ t = pi; % Amount of rotation
 T = [1 0 0 2; 0 1 0 0; 0 0 1 0; 0 0 0 1]; % Initial configuration
 
 %% Travel along a specified screw
+close all
+
 S = m_axang2rotm(s,t/4);
-g = [S (eye(3)-S)*q+h*t*s; 0 0 0 1];
+g = [S (eye(3)-S)*q+h*t/4*s; 0 0 0 1];
 frames = cell(5, 1);
 frames{1} = T;
 for ii = 1:4
     % plotFrame(frames{ii})
-    plotTransforms(se3(frames{ii}), FrameSize=5, FrameAxisLabels="on")
+    plotTransforms(se3(frames{ii}), FrameSize=3, FrameAxisLabels="on")
     hold on
     frames{ii+1} = g*frames{ii};
 end
 % plotFrame(frames{end})
-plotTransforms(se3(frames{end}), FrameSize=5, FrameAxisLabels="on")
+plotTransforms(se3(frames{end}), FrameSize=3, FrameAxisLabels="on")
 T1 = frames{end};
 
 %% Calculate screw to travel from T1 to the origin
@@ -26,6 +30,8 @@ R1 = T1(1:3, 1:3);
 p1 = T1(1:3, 4);
 [ax, ang] = m_rotm2axang(R1');
 h = -p1'*R1*ax/norm(ax)^2/ang;
+ax = sign(h + 1e-16) * ax;
+h = sign(h) * h;
 q = lsqminnorm((eye(3)-R1'), (-R1'*p1-h*ang*ax));
 
 T0 = [R1' (eye(3)-R1')*q + h*ang*ax; 0 0 0 1]*T1;
@@ -33,6 +39,7 @@ plotScrew(q, ax)
 plotTransforms(se3(T1), FrameSize=5, FrameAxisLabels="on")
 hold on
 plotTransforms(se3(T0), FrameSize=5, FrameAxisLabels="on")
+legend()
 % plotFrame(T1)
 
 %% Helper functions
@@ -49,10 +56,10 @@ function plotFrame(T)
 end
 
 function plotScrew(q, s)
-    l = 20;
-    P = [q-s*l q+s*l];
-    plot3(P(1,:), P(2,:), P(3,:), "LineWidth", 3, "Color", "black");
+    l = 5;
+    P = [q-s*l q+s];
+    plot3(P(1,:), P(2,:), P(3,:), "LineWidth", 1, "Color", "black", "DisplayName", "Screw axis");
     hold on
-    scatter3(q(1), q(2), q(3), 100, "filled", "MarkerFaceColor","y")
+    scatter3(q(1), q(2), q(3), 100, "filled", "MarkerFaceColor","y", "DisplayName", "Point on screw axis")
     hold on
 end
